6004b5ed4c94542e1bcd1dbefefcce4538d15d40
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-11-29 15:20:41 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-
-""" Contains makeChange function"""
-
-
+"""Optimized Making Change Algorithm"""
 def makeChange(coins, total):
     """
-    Returns: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any number of coins you have, return -1
+    Calculate fewest number of coins needed to meet total
+    Args:
+        coins: list of coin values
+        total: target sum
+    Returns:
+        Number of coins needed, -1 if impossible, 0 if total <= 0
     """
     if not coins or coins is None:
         return -1
     if total <= 0:
         return 0
+        
+    coins.sort(reverse=True)
     change = 0
-    coins = sorted(coins)[::-1]
+    
     for coin in coins:
         while coin <= total:
             total -= coin
             change += 1
-        if (total == 0):
+        if total == 0:
             return change
+            
     return -1
